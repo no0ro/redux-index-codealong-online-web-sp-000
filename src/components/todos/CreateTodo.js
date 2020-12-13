8b9fb6bc4f478ca7e3c8f42db1242800d463d108
no0ro <nrolwes@gmail.com>
2020-12-13 09:45:27 -0500

    commit tree 438f17d36a7a8638803884c7a765ac752c257ce2

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -13,7 +13,9 @@ class CreateTodo extends Component {
   handleSubmit = event => {
     event.preventDefault();
     this.props.addTodo(this.state)
+    this.setState({text: ''})
   }
+
 
   handleChange(event) {
     this.setState({
