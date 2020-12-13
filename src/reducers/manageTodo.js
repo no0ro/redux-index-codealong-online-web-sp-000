7a4177977a4dccc965368781d257dff357eccd0f
no0ro <nrolwes@gmail.com>
2020-12-13 09:45:35 -0500

    done

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -3,6 +3,7 @@ export default function manageTodo(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_TODO':
+// <<<<<<< HEAD
 
       return { todos: state.todos.concat(action.payload.text) };
 
@@ -10,3 +11,26 @@ export default function manageTodo(state = {
       return state;
   }
 }
+// =======
+//       return { todos: state.todos.concat(action.todo) }
+//     default:
+//       return state;
+//   };
+// };
+// >>>>>>> canon/solution
+
+
+
+
+// export default function manageTodo(state = {
+//  --- todos: [],
+// }, action) {
+//   switch (action.type) {
+//     case 'ADD_TODO':
+
+//       return { todos: state.todos.concat(action.payload.text) };
+
+//     default:
+//       return state;
+//   }
+// }
